69d4ee678dc539e266388152ce24c1f08ca8af8d
jamornP <100953989+jamornP@users.noreply.github.com>
2023-07-25 17:42:57 +0700

    up

--- components/navbar.php
+++ components/navbar.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-123">
   <div class="container">
     <a class="navbar-brand" href="#">
-      <img src="/workshop/imges/logo.png" alt="Logo" style="display:table; margin: 0 auto; max-width:200px;">
+      <img src="/workshop/images/logo.png" alt="Logo" style="display:table; margin: 0 auto; max-width:200px;">
     </a>
     
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
