90a9cebe852aafe4d69cce554714cc0a04bc2332
jamornP <100953989+jamornP@users.noreply.github.com>
2023-07-25 16:48:29 +0700

    up

--- index.php
+++ index.php
@@ -5,20 +5,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Workshop</title>
-
-  <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet"
-    href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
-  <!-- Ionicons -->
-  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="dist/css/adminlte.min.css">
-  <!-- Daterange picker -->
-  <link rel="stylesheet" href="plugins/daterangepicker/daterangepicker.css">
-  <!-- summernote -->
-  <link rel="stylesheet" href="plugins/summernote/summernote-bs4.min.css">
+<?php reque ?>
 </head>
 
 <body class="hold-transition sidebar-mini layout-fixed">
@@ -44,7 +31,7 @@
         </li>
       </ul>
 
-      <!-- Right navbar links -->
+      <!-- Right navbar links --> 
       <ul class="navbar-nav ml-auto">
         <!-- Navbar Search -->
         <li class="nav-item">
