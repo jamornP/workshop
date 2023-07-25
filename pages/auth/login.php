fc2dc246ca0c20fadb8006438d7a67106cbbac0d
arminarm002 <alarm.1993@gmail.com>
2023-07-25 18:18:26 +0700

    arm

--- pages/auth/login.php
+++ pages/auth/login.php
@@ -9,62 +9,59 @@
 </head>
 
 <body class="font-kanit">
-  <?php require $_SERVER['DOCUMENT_ROOT'] . "/workshop/components/navbar.php"; ?>
+  <?php //require $_SERVER['DOCUMENT_ROOT'] . "/workshop/components/navbar.php"; ?>
 
-  <form action="" method="POST">
-    <!-- Email input -->
-    <div class="form-outline mb-4">
-      <input type="email" id="email" class="form-control" />
-      <label class="form-label" for="email">Email address</label>
-    </div>
+  <div class="container" style="margin-top: 10%;">
+    <div class="card" style="max-width: 50%;margin: 0px auto;">
+      <div class="card-header">
+        <h5>
+          Log in
+        </h5>
+      </div>
+      <div class="card-body" style="margin: 0px 10%;">
+        <div class="row">
+            <form action="" method="POST">
+              <!-- Email input -->
+              <div class="form-outline mb-4">
+                <input type="email" id="email" class="form-control" />
+                <label class="form-label" for="email">Email address</label>
+              </div>
 
-    
-    <!-- Password input -->
-    <div class="form-outline mb-4">
-      <input type="password" id="form2Example2" class="form-control" />
-      <label class="form-label" for="form2Example2">Password</label>
-    </div>
+              <!-- Password input -->
+              <div class="form-outline mb-4">
+                <input type="password" id="password" class="form-control" />
+                <label class="form-label" for="password">Password</label>
+              </div>
 
-    <!-- 2 column grid layout for inline styling -->
-    <div class="row mb-4">
-      <div class="col d-flex justify-content-center">
-        <!-- Checkbox -->
-        <div class="form-check">
-          <input class="form-check-input" type="checkbox" value="" id="form2Example31" checked />
-          <label class="form-check-label" for="form2Example31"> Remember me </label>
+              <!-- Submit button -->
+              <button type="button" class="btn btn-primary btn-block mb-4">Sign in</button>
+
+              <!-- Register buttons -->
+              <div class="text-center">
+                <p>Not a member? <a href="register.php">Register</a></p>
+                <!-- <p>or sign up with:</p>
+                <button type="button" class="btn btn-link btn-floating mx-1">
+                  <i class="fab fa-facebook-f"></i>
+                </button> -->
+
+                <button type="button" class="btn btn-link btn-floating mx-1">
+                  <i class="fab fa-google"></i>
+                </button>
+
+                <button type="button" class="btn btn-link btn-floating mx-1">
+                  <i class="fab fa-twitter"></i>
+                </button>
+
+                <button type="button" class="btn btn-link btn-floating mx-1">
+                  <i class="fab fa-github"></i>
+                </button>
+              </div>
+            </form>
         </div>
       </div>
-
-      <div class="col">
-        <!-- Simple link -->
-        <a href="#!">Forgot password?</a>
-      </div>
     </div>
 
-    <!-- Submit button -->
-    <button type="button" class="btn btn-primary btn-block mb-4">Sign in</button>
-
-    <!-- Register buttons -->
-    <div class="text-center">
-      <p>Not a member? <a href="#!">Register</a></p>
-      <p>or sign up with:</p>
-      <button type="button" class="btn btn-link btn-floating mx-1">
-        <i class="fab fa-facebook-f"></i>
-      </button>
-
-      <button type="button" class="btn btn-link btn-floating mx-1">
-        <i class="fab fa-google"></i>
-      </button>
-
-      <button type="button" class="btn btn-link btn-floating mx-1">
-        <i class="fab fa-twitter"></i>
-      </button>
-
-      <button type="button" class="btn btn-link btn-floating mx-1">
-        <i class="fab fa-github"></i>
-      </button>
-    </div>
-  </form>
+  </div>
 
 
   <?php require $_SERVER['DOCUMENT_ROOT'] . "/workshop/components/script.php"; ?>
