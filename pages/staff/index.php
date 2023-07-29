5380eea376c5bbd3bc4756c987260130d27e4552
jamornP <100953989+jamornP@users.noreply.github.com>
2023-07-29 13:06:02 +0700

    up

--- pages/staff/index.php
+++ pages/staff/index.php
@@ -2,15 +2,15 @@
 <html lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Workshop</title>
-  <?php require $_SERVER['DOCUMENT_ROOT'] . "/workshop/components/link.php"; ?>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Workshop</title>
+    <?php require $_SERVER['DOCUMENT_ROOT'] . "/workshop/components/link.php"; ?>
 </head>
 
 <body class="font-kanit">
-  <?php require $_SERVER['DOCUMENT_ROOT'] . "/workshop/components/navbar.php"; ?>
-  <?php
+    <?php require $_SERVER['DOCUMENT_ROOT'] . "/workshop/components/navbar.php"; ?>
+    <?php
     if ($_SESSION['role'] == 'admin' OR $_SESSION['role'] == 'staff') {
       $dateN = date("Y-m-d");
       $timeN = date("H:i:s");
@@ -62,35 +62,35 @@
           }
         }
         ?>
-          <div class="container mt-3">
-            <div class="card">
-              <div class="card-header bg-primary text-white text-center">
+    <div class="container mt-3">
+        <div class="card">
+            <div class="card-header bg-primary text-white text-center">
                 <h5><b class='fs-24'>กิจกรรม</b><br> <?php echo $w_name; ?></h5>
-              </div>
-              <div class="card-body">
+            </div>
+            <div class="card-body">
                 <button type="button" class="btn btn-success position-relative text-white">
-                  <?php echo $round; ?>
-                  <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-                    <?php
+                    <?php echo $round; ?>
+                    <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                        <?php
 
                     echo $incom;
                     ?>
-                    <span class="visually-hidden"></span>
-                  </span>
+                        <span class="visually-hidden"></span>
+                    </span>
                 </button>
                 <div class="mt-2">สถานที่ : <?php echo $department; ?></div>
                 <div id="divtime"></div>
                 <table class='table table-bordered'>
-                  <thead>
-                    <tr class='table-success text-center'>
-                      <th>ที่</th>
-                      <th>ชื่อ-สกุล</th>
-                      <th>เวลาเข้า</th>
-                      <th>ลบ</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <?php
+                    <thead>
+                        <tr class='table-success text-center'>
+                            <th>ที่</th>
+                            <th>ชื่อ-สกุล</th>
+                            <th>เวลาเข้า</th>
+                            <th>ลบ</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
                     $i = 0;
 
                     $dataWS = $dataObj->getDataByWId("data", $wd_id);
@@ -113,34 +113,34 @@
 
                     ?>
 
-                  </tbody>
+                    </tbody>
                 </table>
-              </div>
-
             </div>
-          </div>
-        <?php
+
+        </div>
+    </div>
+    <?php
       } else {
         $dataW = $dataObj->getWorkshopBySt($_SESSION['s_id']);
         // print_r($dataW);
         ?>
-        <div class="container mt-3">
-          <div class="card text-center">
+    <div class="container mt-3">
+        <div class="card text-center">
             <div class="card-header bg-primary text-white text-center">
-              <h5><b class='fs-24'>กิจกรรม</b><br> <?php echo $dataW['w_name']; ?></h5>
+                <h5><b class='fs-24'>กิจกรรม</b><br> <?php echo $dataW['w_name']; ?></h5>
             </div>
             <div class="card-body">
-              <button type="button" class="btn btn-success position-relative text-white">
-                <?php echo $dataW['w_detail']; ?>
-                <!-- <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                <button type="button" class="btn btn-success position-relative text-white">
+                    <?php echo $dataW['w_detail']; ?>
+                    <!-- <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
                   <?php //echo $dataW['wd_amount']; ?>
                   <span class="visually-hidden"></span>
                 </span> -->
-              </button>
-              
-              <div class="mt-2">สถานที่ : <?php echo $dataW['wd_address'] ?></div>
-              <div id="divtime"></div>
-              <?php 
+                </button>
+
+                <div class="mt-2">สถานที่ : <?php echo $dataW['wd_address'] ?></div>
+                <div id="divtime"></div>
+                <?php 
                 $dataWD = $dataObj->getWorkshopBySt2($_SESSION['s_id']);
                 foreach($dataWD as $wd){
                   $round = $wd['wd_round'] ." วันที่ ".datethai($wd['wd_date']). " เวลา " . time_sort($wd['wd_time_start']) . " - " . time_sort($wd['wd_time_end']);
@@ -175,14 +175,14 @@
                   ";
                 }
               ?>
-              <div class="mt-5">
-                <h4 class="text-center text-danger"><b><?php echo $text;?></b></h4>
-              </div>
+                <div class="mt-5">
+                    <h4 class="text-center text-danger"><b><?php echo $text;?></b></h4>
+                </div>
             </div>
 
-          </div>
         </div>
-        <?php
+    </div>
+    <?php
       }
     }else{
       
@@ -197,50 +197,50 @@
     }
   ?>
 
-  <?php require $_SERVER['DOCUMENT_ROOT'] . "/workshop/components/script.php"; ?>
-  <script>
+    <?php require $_SERVER['DOCUMENT_ROOT'] . "/workshop/components/script.php"; ?>
+    <script>
     function showClockRealTime() {
         var d = new Date();
-        if(d.getHours()<10){
-          var h = "0"+d.getHours();
-        }else{
-          var h = d.getHours();
+        if (d.getHours() < 10) {
+            var h = "0" + d.getHours();
+        } else {
+            var h = d.getHours();
         }
-        if(d.getMinutes()<10){
-          var m = "0"+d.getMinutes();
-        }else{
-          var m = d.getMinutes();
+        if (d.getMinutes() < 10) {
+            var m = "0" + d.getMinutes();
+        } else {
+            var m = d.getMinutes();
         }
-        if(d.getSeconds()<10){
-          var s = "0"+d.getSeconds();
-        }else{
-          var s = d.getSeconds();
+        if (d.getSeconds() < 10) {
+            var s = "0" + d.getSeconds();
+        } else {
+            var s = d.getSeconds();
         }
-        document.getElementById("divtime").innerHTML = "เวลาปัจจุบัน คือ "+h+":"+m+":"+s;
+        document.getElementById("divtime").innerHTML = "เวลาปัจจุบัน คือ " + h + ":" + m + ":" + s;
     }
     setInterval("showClockRealTime()", 1000);
-  </script>
-      <script>
-        // Command: toastr["success"]("ข้อความ")
+    </script>
+    <script>
+    // Command: toastr["success"]("ข้อความ")
 
-        //ตัวนี้จะเอาไว้ set ค่าต่างๆ toastr
-        toastr.options = {
-            "closeButton": true,
-            "debug": false,
-            "newestOnTop": false,
-            "progressBar": false,
-            "positionClass": "toast-top-right",
-            "preventDuplicates": false,
-            "onclick": null,
-            "showDuration": "300",
-            "hideDuration": "1000",
-            "timeOut": "3000",
-            "extendedTimeOut": "1000",
-            "showEasing": "swing",
-            "hideEasing": "linear",
-            "showMethod": "fadeIn",
-            "hideMethod": "fadeOut"
-        }
+    //ตัวนี้จะเอาไว้ set ค่าต่างๆ toastr
+    toastr.options = {
+        "closeButton": true,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": false,
+        "positionClass": "toast-top-right",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "3000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    }
     </script>
 </body>
 
